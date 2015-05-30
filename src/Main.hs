0d44ea184a8c46bb0cf427d2ab60f88d918e01b3
QuentinDuval <senmenty@gmail.com>
2015-05-30 09:35:25 +0200

    First successful connection

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,10 @@ module Main where
 
 
 -- import Control.Lens
+import Control.Applicative
 import Control.Monad.IO.Class(liftIO)
 
+import qualified Data.ByteString as B
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
 import qualified Data.Text as T
@@ -12,40 +14,30 @@ import qualified Data.Text.IO as T
 
 import Data.List (isInfixOf, or)
 import Data.Map((!))
+import Data.String
 import Data.Text(Text)
 
-import Network.HTTP
 import Network.HTTP.Conduit
+import System.Environment
+import System.IO
+
 import Web.Authenticate.OAuth
 import Web.Twitter.Conduit
 import Web.Twitter.Types
 
 
 
--- DOC
--- http://hackage.haskell.org/package/twitter-conduit-0.1.0/docs/Web-Twitter-Conduit.html#
-
 
 main :: IO()
 main = do
-   let tokens = twitterOAuth { oauthConsumerKey = "YOUR CONSUMER KEY" , oauthConsumerSecret = "YOUR CONSUMER SECRET" }
-   let credential = Credential
-         [ ("oauth_token", "YOUR ACCESS TOKEN"), ("oauth_token_secret", "YOUR ACCESS TOKEN SECRET") ]
+   keyFilePath <- head <$> getArgs
+   keys <- lines <$> readFile keyFilePath
+   let [k1, k2, k3, k4] = fmap fromString keys
+   let tokens = twitterOAuth { oauthConsumerKey = k1, oauthConsumerSecret = k2 }
+   let credential = Credential [ ("oauth_token", k3), ("oauth_token_secret", k4) ]
    let twitterInfo = setCredential tokens credential def
-
    timeline <- withManager $ \m -> call twitterInfo m homeTimeline
                                    -- call twitterInfo mgr $ homeTimeline & count ?~ 200
    print timeline
-   -- analyze "haskell"
 
 
-{-
-analyze :: Text -> IO()
-analyze query = runTwitterFromEnv' $ do
-   src <- stream $ statusesFilterByTrack query
-   src C.$$+- CL.mapM_ (liftIO . process)
-
-
-process :: StreamingAPI -> IO ()
-process = undefined
--}
