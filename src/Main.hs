889f8687379bd0659dfbd1a62237d42d7d8ac553
QuentinDuval <senmenty@gmail.com>
2015-05-30 09:54:08 +0200

    Minor

--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,10 @@ module Main where
 
 -- import Control.Lens
 import Control.Applicative
+import Control.Lens
 import Control.Monad.IO.Class(liftIO)
 
-import qualified Data.Conduit as C
+import Data.Conduit as C
 import qualified Data.Conduit.List as CL
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -19,9 +20,10 @@ import Data.Text(Text)
 import Network.HTTP.Conduit
 import System.Environment
 import Web.Twitter.Conduit
-import Web.Twitter.Types
+import Web.Twitter.Types.Lens
 
 import TwitterAuth
+
 
 
 
@@ -29,8 +31,13 @@ main :: IO()
 main = do
    keyFilePath <- head <$> getArgs
    twitterInfo <- fromFile keyFilePath
-   timeline <- withManager $ \m -> call twitterInfo m homeTimeline
-                                   -- call twitterInfo mgr $ homeTimeline & count ?~ 200
+   
+   timeline <- withManager $ \m -> 
+      sourceWithMaxId twitterInfo m homeTimeline
+         $= CL.isolate 60
+         $$ CL.mapM_ $ \status ->
+            liftIO $ T.putStrLn (status ^. statusUser . userScreenName)
+   
    print timeline
 
 
