f33d899e6ed901ba5d93cb21df9936561957b649
QuentinDuval <senmenty@gmail.com>
2015-05-30 09:56:05 +0200

    Minor

--- src/Main.hs
+++ src/Main.hs
@@ -26,18 +26,13 @@ import TwitterAuth
 
 
 
-
 main :: IO()
 main = do
-   keyFilePath <- head <$> getArgs
-   twitterInfo <- fromFile keyFilePath
-   
-   timeline <- withManager $ \m -> 
+   twitterInfo <- fromFile =<< head <$> getArgs
+   withManager $ \m -> 
       sourceWithMaxId twitterInfo m homeTimeline
          $= CL.isolate 60
          $$ CL.mapM_ $ \status ->
             liftIO $ T.putStrLn (status ^. statusUser . userScreenName)
-   
-   print timeline
 
 
