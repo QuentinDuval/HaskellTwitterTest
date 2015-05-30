4d351fe17764b5b217672e83094fde8c671bbbc5
QuentinDuval <senmenty@gmail.com>
2015-05-30 10:04:55 +0200

    Filtering specific users

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,16 @@
 module Main where
 
 
--- import Control.Lens
 import Control.Applicative
 import Control.Lens
 import Control.Monad.IO.Class(liftIO)
 
 import Data.Conduit as C
 import qualified Data.Conduit.List as CL
+
+import Data.Text(Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-
-import Data.List (isInfixOf, or)
-import Data.Map((!))
-import Data.String
-import Data.Text(Text)
 
 import Network.HTTP.Conduit
 import System.Environment
@@ -31,7 +27,13 @@ main = do
    logInfo <- fromFile =<< head <$> getArgs
    withManager $ \m -> 
       sourceWithMaxId logInfo m homeTimeline
-         $$ CL.isolate 60 =$ CL.mapM_ (liftIO . handleTweet)
+         $$ CL.filter (filterUser "reddit_haskell")
+         $= CL.isolate 10 =$ CL.mapM_ (liftIO . handleTweet)
+
+
+filterUser :: Text -> Status -> Bool
+filterUser name' status =
+   status ^. statusUser . userScreenName == name' 
 
 
 handleTweet :: Status -> IO ()
